6a47ca18d1accd9f20ddc078d5b2f5f7b228b0d7
Sean McDermott <smcdermo@chicagobooth.edu>
2017-07-25 18:46:13 +0000

    few things done

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,6 +4,15 @@ class PicturesController < ApplicationController
     end
     
     def create_row
+        # Parameters: {"the_source"=>"", "the_caption"=>""}
+        
+        p = Photo.new
+        p.source = params["the_source"]
+        p.caption = params["the_caption"]
+        p.save
+        
+        @photo_count = Photo.count
+        
         render("pic_templates/create_row.html.erb")
     end
     
